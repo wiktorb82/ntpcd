ea8863a97583858e3f82ef37711dfb0f700f33ee
Wiktor Bogacki <you@example.com>
2014-05-19 21:53:21 +0200

    forgot one header

---
+++ ntpdc.h
@@ -0,0 +1,18 @@
+/*
+*Some common stuff
+*
+*
+*
+*/
+#ifndef _NTPDC_H
+
+struct send_stamp
+{
+        double  T1;
+        double  T2;
+        double  T3;
+        double  T4;
+}typedef send_stamp;
+
+
+#endif
