bea6d8869f4fea4fa2a092440fc8056126ba84d9
Wiktor Bogacki <you@example.com>
2014-05-19 21:21:25 +0200

    fixed autoprojects mess

--- udpc.c
+++ udpc.c
@@ -25,7 +25,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <getopt.h>
-#include "system.h"
+#include "ntpdc.h"
 #include <stdio.h>
 #include <sys/types.h>
 #include <getopt.h>
@@ -44,36 +44,11 @@
 
 #define EXIT_FAILURE 1
 
-char *xmalloc ();
-char *xrealloc ();
-char *xstrdup ();
-
 void diep(char *s)
 {
         perror(s);
         exit(1);
 }
-
-
-static void usage (int status);
-
-/* The name the program was run with, stripped of any leading path. */
-char *program_name;
-
-/* getopt_long return codes */
-enum {DUMMY_CODE=129
-     };
-
-/* Option flags and variables */
-
-
-static struct option const long_options[] = {
-        {"help", no_argument, 0, 'h'},
-        {"version", no_argument, 0, 'V'},
-        {NULL, 0, NULL, 0}
-};
-
-static int decode_switches (int argc, char **argv);
 
 int
 main (int argc, char **argv)
@@ -110,48 +85,4 @@ main (int argc, char **argv)
         close(s);
 
         return 0;
-}
-
-/* Set all the option flags according to the switches specified.
-   Return the index of the first non-option argument.  */
-
-static int
-decode_switches (int argc, char **argv)
-{
-        int c;
-
-
-        while ((c = getopt_long (argc, argv,
-                                 "h"	/* help */
-                                 "V",	/* version */
-                                 long_options, (int *) 0)) != EOF) {
-                switch (c) {
-                case 'V':
-                        printf ("udpdc %s\n", VERSION);
-                        exit (0);
-
-                case 'h':
-                        usage (0);
-
-                default:
-                        usage (EXIT_FAILURE);
-                }
-        }
-
-        return optind;
-}
-
-
-static void
-usage (int status)
-{
-        printf (_("%s - \
-server and client\n"), program_name);
-        printf (_("Usage: %s [OPTION]... [FILE]...\n"), program_name);
-        printf (_("\
-Options:\n\
-  -h, --help                 display this help and exit\n\
-  -V, --version              output version information and exit\n\
-"));
-        exit (status);
 }
--- udpd.c
+++ udpd.c
@@ -25,7 +25,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <getopt.h>
-#include "system.h"
+#include "ntpdc.h"
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -40,30 +40,6 @@
 
 #define EXIT_FAILURE 1
 
-char *xmalloc ();
-char *xrealloc ();
-char *xstrdup ();
-
-
-static void usage (int status);
-
-/* The name the program was run with, stripped of any leading path. */
-char *program_name;
-
-/* getopt_long return codes */
-enum {DUMMY_CODE=129
-     };
-
-/* Option flags and variables */
-
-
-static struct option const long_options[] = {
-        {"help", no_argument, 0, 'h'},
-        {"version", no_argument, 0, 'V'},
-        {NULL, 0, NULL, 0}
-};
-
-static int decode_switches (int argc, char **argv);
 void diep(char *s)
 {
         perror(s);
@@ -113,48 +89,4 @@ main (int argc, char **argv)
 
         close(s);
         return 0;
-}
-
-/* Set all the option flags according to the switches specified.
-   Return the index of the first non-option argument.  */
-
-static int
-decode_switches (int argc, char **argv)
-{
-        int c;
-
-
-        while ((c = getopt_long (argc, argv,
-                                 "h"	/* help */
-                                 "V",	/* version */
-                                 long_options, (int *) 0)) != EOF) {
-                switch (c) {
-                case 'V':
-                        printf ("udpdc %s\n", VERSION);
-                        exit (0);
-
-                case 'h':
-                        usage (0);
-
-                default:
-                        usage (EXIT_FAILURE);
-                }
-        }
-
-        return optind;
-}
-
-
-static void
-usage (int status)
-{
-        printf (_("%s - \
-server and client\n"), program_name);
-        printf (_("Usage: %s [OPTION]... [FILE]...\n"), program_name);
-        printf (_("\
-Options:\n\
-  -h, --help                 display this help and exit\n\
-  -V, --version              output version information and exit\n\
-"));
-        exit (status);
 }
