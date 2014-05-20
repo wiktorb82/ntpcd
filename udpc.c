9207ea319db2c3780906690f660121a6a44d8f96
Wiktor Bogacki <you@example.com>
2014-05-20 09:26:48 +0200

    Cleanup

--- udpc.c
+++ udpc.c
@@ -1,27 +1,3 @@
-/*
-   udpdc - server and client
-
-   Copyright (C) 2014 virtualbox
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
-#include <termios.h>
-#include <grp.h>
-#include <pwd.h>
-*/
-
 #include <stdio.h>
 #include <sys/types.h>
 #include <getopt.h>
@@ -36,13 +12,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define BUFLEN 512
 #define NUMPACKS 5
 #define PORT 12345
 #define SRV_IP "127.0.0.1"
-
-
-#define EXIT_FAILURE 1
 
 void diep(char *s)
 {
@@ -60,11 +32,14 @@ main (int argc, char **argv)
         int s, i, slen=sizeof(si_other);
 
         if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+	{
                 diep("socket");
+	}
 
         memset((char *) &si_other, 0, sizeof(si_other));
         si_other.sin_family = AF_INET;
         si_other.sin_port = htons(PORT);
+
         if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
                 fprintf(stderr, "inet_aton() failed\n");
                 exit(1);
